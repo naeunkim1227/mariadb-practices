a6f66c9539578f305be838ca3edfa5319f4400a1
naeunkim1227 <kne0140@daum.net>
2021-10-05 17:21:49 +0900

    21.10.05

---
+++ jdbc-pratices/src/main/java/test/DeleteTest01.java
@@ -0,0 +1,65 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteTest01 {
+
+	public static void main(String[] args) {
+		Boolean result =  delete(5L);
+		System.out.println(result? "성공" : "실패");
+
+	}
+
+	private static Boolean delete(long n) {
+		boolean result = false;
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+			//1. JDBC 드라이버 로드
+			Class.forName("org.mariadb.jdbc.Driver");
+		
+			//2. 연결하기
+			String url = "jdbc:mysql://127.0.0.1:3306/employees?charset=utf8";
+			conn= DriverManager.getConnection(url, "hr" , "hr");
+			//url, 아이디, 비밀번호
+			
+			System.out.println("연결성공");
+			
+			//3.statement 생성
+			stmt = conn.createStatement();
+			
+			//4. SQL실행
+			String sql = "delete from dept where no=" + n;
+			int count = stmt.executeUpdate(sql);
+			
+			//count가 1일때 true로 변경
+			result = count == 1;
+		
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("드라이버 로딩 실패" + e);
+		}catch (SQLException e) {
+			System.out.println("SQL 예외 발생 error:" + e);
+		}finally {
+			
+			//clean up 자원정리
+			try {
+				stmt.close();
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+		
+	}
+
+}
