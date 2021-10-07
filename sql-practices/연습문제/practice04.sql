9617d5bd013f8b6de3e2aea1f75dd40e161fe883
naeunkim1227 <kne0140@daum.net>
2021-10-07 20:20:41 +0900

    21.10.07 쿼리 연습문제 완료

--- sql-practices/연습문제/practice04.sql
+++ sql-practices/연습문제/practice04.sql
@@ -38,34 +38,7 @@ join (select b.dept_no,avg(a.salary) as 'avgsal'
 where b.salary > d.avgsal
 and b.to_date = '9999-01-01'
 and c.to_date = '9999-01-01';
-
-
--- 정답 풀이
-SELECT 
-    a.emp_no, CONCAT(a.first_name, ' ', a.last_name), b.salary
-FROM
-    employees a,
-    salaries b,
-    dept_emp c,
-    (SELECT 
-        c.dept_no, AVG(b.salary) AS avg_salary
-    FROM
-        employees a, salaries b, dept_emp c
-    WHERE
-        a.emp_no = b.emp_no
-            AND a.emp_no = c.emp_no
-            AND b.to_date = '9999-01-01'
-            AND c.to_date = '9999-01-01'
-    GROUP BY c.dept_no) d
-WHERE
-    a.emp_no = b.emp_no
-        AND a.emp_no = c.emp_no
-        AND c.dept_no = d.dept_no
-        AND b.salary > d.avg_salary
-        AND b.to_date = '9999-01-01'
-        AND c.to_date = '9999-01-01';                                   
-                                    
-                                    
+                                
 
 -- 문제4.현재, 사원들의 사번, 이름, 매니저 이름, 부서 이름으로 출력해 보세요.
 select a.emp_no, concat(a.first_name,' ' ,a.last_name) as '이름', e.mng_name as '매니저 이름', d.dept_name as '부서 이름'
@@ -130,38 +103,7 @@ WHERE
 			group by b.dept_no order by avg(a.salary) desc limit 0,1) a)
     order by c.salary desc;        
 	
-								
- -- 정답
- SELECT 
-    a.emp_no,
-    CONCAT(a.first_name, ' ', a.last_name) AS name,
-    b.title,
-    c.salary
-FROM
-    employees a,
-    titles b,
-    salaries c,
-    dept_emp d
-WHERE
-    a.emp_no = b.emp_no
-        AND a.emp_no = c.emp_no
-        AND a.emp_no = d.emp_no
-        AND b.to_date = '9999-01-01'
-        AND c.to_date = '9999-01-01'
-        AND d.to_date = '9999-01-01'
-        AND d.dept_no = (SELECT dept_no
-FROM (SELECT dept_no, AVG(salary) AS avg_salary
-            FROM
-                salaries a, dept_emp b
-            WHERE
-                a.emp_no = b.emp_no
-                    AND a.to_date = '9999-01-01'
-                    AND b.to_date = '9999-01-01'
-            GROUP BY dept_no
-            ORDER BY avg_salary DESC
-            LIMIT 0 , 1) a)
-ORDER BY c.salary DESC;
- 
+							
 -- 문제6.평균 연봉이 가장 높은 부서는? 
 -- 1)평균연봉 내림차순 쿼리
 SELECT  MAX(avg_salary) FROM (SELECT 
@@ -196,35 +138,6 @@ having avg(c.salary) = (SELECT  MAX(avg_salary)
 										AND c.to_date = '9999-01-01'
 								GROUP BY c.dept_no) a);
 
-
--- 답
-SELECT 
-    d.dept_name, ROUND(AVG(b.salary)) AS avg_salary
-FROM
-    employees a,
-    salaries b,
-    dept_emp c,
-    departments d
-WHERE
-    a.emp_no = b.emp_no
-        AND a.emp_no = c.emp_no
-        AND c.dept_no = d.dept_no
-        AND b.to_date = '9999-01-01'
-        AND c.to_date = '9999-01-01'
-GROUP BY c.dept_no
-HAVING avg_salary = (SELECT 
-        MAX(avg_salary)
-    FROM
-        (SELECT 
-            ROUND(AVG(b.salary)) AS avg_salary
-        FROM
-            employees a, salaries b, dept_emp c
-        WHERE
-            a.emp_no = b.emp_no
-                AND a.emp_no = c.emp_no
-                AND b.to_date = '9999-01-01'
-                AND c.to_date = '9999-01-01'
-        GROUP BY c.dept_no) a);
 -- 문제7.평균 연봉이 가장 높은 직책?
 -- 1) 평균연봉
 select max(avg_salary) from (select avg(salary) as avg_salary
@@ -234,6 +147,8 @@ select max(avg_salary) from (select avg(salary) as avg_salary
                     and b.to_date = '9999-01-01'
                     group by b.title) a;
 
+                    
+ -- 최종 쿼리                   
 select a.title, avg(b.salary)
 from titles a join salaries b on a.emp_no = b.emp_no
 where a.to_date = '9999-01-01' and b.to_date = '9999-01-01'
@@ -250,19 +165,6 @@ having avg(b.salary) = (select max(avg_salary) from
 -- 부서이름, 사원이름, 연봉, 매니저 이름, 메니저 연봉 순으로 출력합니다.
 
 -- 1) 매니저 연봉
-select f.dept_name, a.first_name, d.salary , g.first_name , e.salary
-from employees a 
-join dept_emp b on a.emp_no = b.emp_no
-join dept_manager c on c.emp_no = a.emp_no
-join salaries d on a.emp_no = d.emp_no
-join salaries e on a.emp_no = d.emp_no
-join departments f on f.dept_no =b.dept_no
-join employees g on c.emp_no = g.emp_no
-where d.salary > e.salary;
-
-
-
--- 매니저 연봉
 select a.emp_no as '매니저 사번 ', b.emp_no as '사원 사번' , c.salary as '매니저 월급' , d.salary as '사원 월급'
 from dept_manager a
 join dept_emp b on a.dept_no = b.dept_no
@@ -271,6 +173,7 @@ join salaries d on d.emp_no = b.emp_no;
 
 
 -- 부서이름, 사원이름, 연봉, 매니저 이름, 메니저 연봉
+-- 답!!!
 select  a.emp_no as '사번', a.first_name as '사원 이름' , c.salary as '사원 연봉' , b.first_name as '매니저 이름', d.salary as '매니저 연봉'
 from employees a, -- 직원
 employees b, -- 매니저
