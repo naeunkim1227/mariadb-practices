9f190c49a0bf4180d20fe457b5465037814d74a3
naeunkim1227 <kne0140@daum.net>
2021-10-08 18:43:28 +0900

    21.10.08

--- bookshop.sql
+++ bookshop.sql
@@ -4,14 +4,16 @@ desc book;
 desc author;
 
 -- insert author
-
-
-
-
+insert into author values (null, '스테파니 메이어');
 -- insert book
+insert into book values (null, '이클립스', 1 , '대여가능');
 
 -- select book
+select a.no , a.title , a.status , b.name as authorname
+from book a, author b
+where a.author_no = b.no;
+
 
 -- update book's status(no,status)
-
+update book set status = '대여중' where no=1;
 
